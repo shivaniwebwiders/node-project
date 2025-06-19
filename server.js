d01d483773f5fd64671e047bc90cde59055eabb0
shivani <shivania.webwiders@gmail.com>
2025-06-19 18:56:38 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
